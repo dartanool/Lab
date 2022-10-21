c25ff1c2d54bfc6c2be2c96d7b6a91514d9e014f
Александра Дартан-оол <dartanool2003@gmail.com>
2022-10-21 10:20:37 +0300

    task 1

---
+++ Lab_1/Lab1z1/Lab1z1/Lab1z1.cpp
@@ -0,0 +1,14 @@
+﻿
+#include <iostream>> //Подкючает библиотеку input-output stream для вывода и ввода в консоль
+#include <Windows.h>
+//#include <Windows.h>
+using namespace std;
+
+int main() //Главная функция с типом integer 
+{ // Фигурными скобками выделяется тело функции 
+    setlocale(LC_ALL, ""); // Вывод в консоль русского языка
+    cout << "First string" << endl;
+    cout << "Первая строка" << endl << "Вторая строка" << endl;
+    cout << "Спецсимволы  \" \\ \" " << endl;
+
+}
