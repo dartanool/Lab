6e790e0e827869b56a93e9a48c00146e279f936e
Александра Дартан-оол <dartanool2003@gmail.com>
2022-10-21 10:04:23 +0300

    commit

---
+++ Lab_3/Lab3z1z2/Lab3z1z2/Lab3z1z2.cpp
@@ -0,0 +1,72 @@
+﻿// 3 Лабораторная работа.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using std::cout;
+using std::endl;
+
+
+int sum(int a, int b) {
+    return a + b;
+}
+int sub(int a, int b) {
+    return a - b;
+}
+int printArray(int* begin, int* end) {
+    int sum = 0;
+    for (int* p = begin; p < end; p++) {
+        sum += *p;
+    }
+    return sum;
+}
+
+int fun(char a, int x, int y)
+{
+    if (a == '+')
+        return sum(x, y);
+    if (a == '-')
+        return sub(x, y);
+}
+
+
+int main()
+{
+    setlocale(LC_ALL, "");
+    //ЗАДАНИЕ 1
+    cout << "ЗАДАНИЕ 1 " << endl;
+    int* b;
+    int a = 10;
+    b = &a;
+    cout << " Адрес Указателя = " << *b << endl;
+    *b += 2;
+    cout << " Измененное значение Указателя = " << *b << endl;
+    cout << "a = " << a << endl;
+    int Arr[] = { 1,2,3,4 };
+    for (int i = 0; i < 4; i++) {
+
+        std::cout << *(Arr + i) << "\n";
+
+    }
+    int k = 56;
+    int* const d = &k;
+    cout << "До изменения  = " << k << endl;
+    *d = 3;
+    cout << "После изменения = " << k << endl;
+
+    //ЗАДАНИЕ 2
+    cout << "ЗАДАНИЕ 2 " << endl;
+    int A[] = { 5, 6, 7, 8, 9, 10 };
+    int* begin = std::begin(A);
+    int* end = std::end(A);
+    cout << "Сумма элементов массива  = ";
+    cout << printArray(begin, end) << endl;
+    cout << fun('+', 129, 5) << endl;
+
+
+    float* pc1 = new float(314.21);
+    cout << *pc1 << endl;;
+    delete pc1;
+
+
+
+}
