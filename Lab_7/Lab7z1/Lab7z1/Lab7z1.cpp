5a20d5a6dde1cce80700f821cbfd41977a4f64a0
Александра Дартан-оол <dartanool2003@gmail.com>
2022-11-24 17:55:00 +0300

    task 1

---
+++ Lab_7/Lab7z1/Lab7z1/Lab7z1.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+void print(string q) 
+{
+	cout << q << endl;
+}
+
+int main()
+{
+	cout << "Please, in English " << endl;
+	string q;
+	cin >> q;
+	print(q.substr(2, 3));
+	
+	int index = q.find("a");
+	print(to_string(index));
+	if (index == 0) cout << "Символа а не найдено" << endl;
+
+}
+
