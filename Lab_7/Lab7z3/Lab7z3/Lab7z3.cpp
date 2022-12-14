d5acc630006cdf37f5d0c4209553e8d392848636
Александра Дартан-оол <dartanool2003@gmail.com>
2022-12-14 18:08:07 +0300

    task3

--- Lab_7/Lab7z3/Lab7z3/Lab7z3.cpp
+++ Lab_7/Lab7z3/Lab7z3/Lab7z3.cpp
@@ -1,10 +1,36 @@
 ﻿#define _USE_MATH_DEFINES
 #include <iostream>
 #include <fstream>
+#include <string>
 using namespace std;
+
+
+void read()
+{
+    fstream qwer("a.txt");
+    string line;
+    while (getline(qwer, line)) cout << line  << '\n';
+    qwer.close();
+}
+
+void erase()
+{
+    fstream qwer;
+    qwer.open("a.txt", fstream::out | fstream::trunc);
+    qwer.close();
+}
+
+void write(string& asd)
+{
+    fstream qwer;
+    qwer.open("a.txt", ios::app);
+    qwer<< asd << '\n';
+    qwer.close();
+}
 
 int main()
 {
+
     setlocale(LC_ALL, "");
     cout<< "Введите радиус окружности " << endl;
     float q;
@@ -13,9 +39,32 @@ int main()
     float e = trunc(w * 1000 + 0.5) / 1000;
     cout << w << endl;
     cout << "Введите произвольну. строку " << endl;
-    string r;
-    cin >> r;
+    string asd;
+    cin >> asd;
     fstream qwer;
-    qwer.open("D:\\Git_Lab\\Lab_7\\Lab7z3\\a.txt", std::ios::out);
+
+    string qwe;
+    qwer.open("a.txt", ios::in);
+    qwer >> qwe;
+    cout << qwe << endl;
     qwer.close();
+    /*    qwer.open("a.txt", ios::in);
+    string data;
+    string line;
+    while (getline(qwer, line)) {
+
+        data += line + "\n";
+
+    }
+    qwer << q<< endl;
+    cout << data << endl;
+    qwer.close();*/
+
+    
+    while (true) {
+        if (asd == "exit") break;
+        else if (asd == "read") read();
+        else if (asd == "erase") erase();
+        else write(asd);
+    }
 }
