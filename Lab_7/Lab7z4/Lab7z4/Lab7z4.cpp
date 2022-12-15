050a512776972eb2c11760abc63e70df659593b9
Александра Дартан-оол <dartanool2003@gmail.com>
2022-12-15 19:41:59 +0300

    very hard

---
+++ Lab_7/Lab7z4/Lab7z4/Lab7z4.cpp
@@ -0,0 +1,69 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+using namespace std;
+
+class Settings
+{
+private:
+	static map <string, int> map;
+
+public:
+
+	static void add(string q, int w)
+	{
+		map.insert(make_pair(q, w));
+	}
+
+	static int get(string q)
+	{
+		return map[q];
+	}
+
+	static void print()
+	{
+		cout << "map: " << endl;
+		for (auto i = map.begin(); i != map.end(); i++) {
+
+			cout << i->first << " = " << i->second << "\n";
+		}
+	}
+
+
+};
+
+void print(vector<float> v)
+{
+	cout << "Vector: ";
+	for (float n : v) cout << n << " ";
+	cout << endl;
+}
+
+int main()
+{
+	vector<float> v = { 1.1,2.2,3.3,4.4,5.5 };
+	print(v);
+
+	auto iter = v.cbegin();
+	v.emplace(iter + 3, 1.2);
+	print(v);
+
+	v.pop_back();
+	print(v);
+
+
+	/*
+	Settings q("wertyuio", 11);
+	q.add("sdf", 21);
+	q.add("fyvh", 13);
+	q.print();
+	cout<<  q.get("fyvh")<< endl;*/
+
+
+	Settings mp;
+	mp.add("qwert", 567);
+	mp.add("qwerty", 789);
+	mp.print();
+	cout <<  mp.get("qwert")<<endl;
+}
